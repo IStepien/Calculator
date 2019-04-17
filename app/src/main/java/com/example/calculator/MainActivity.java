b9cd549ef5c853309d4986ff17cf5a83c4cd0520
Irmina Stepien <irm.stepien@gmail.com>
2019-04-17 13:33:38 +0200

    Data binding library added. Binding for buttons added.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,13 +1,107 @@
 package com.example.calculator;
 
+
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.calculator.databinding.MainActivityBinding;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainActivityBinding binding;
+
     @Override
-    public void onCreate( Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"1");
+            }
+        });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"2");
+            }
+        });
+
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"3");
+            }
+        });
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"4");
+            }
+        });
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"5");
+            }
+        });
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"6");
+            }
+        });
+        binding.button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"7");
+            }
+        });
+        binding.button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"8");
+            }
+        });
+        binding.button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"9");
+            }
+        });
+
+        binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"+");
+            }
+        });
+
+        binding.buttonDivid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"/");
+            }
+        });
+        binding.buttonMultipl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"*");
+            }
+        });
+        binding.buttonSubtr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.textViewResult.setText(binding.textViewResult.getText()+"-");
+            }
+        });
+
+
+
     }
 }
